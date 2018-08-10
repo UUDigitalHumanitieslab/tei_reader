4b7d11d64005f6f11b15b72eb3695dd1820d36ff
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-08-10 17:17:20 +0200

    0.0.13

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='tei_reader',
     python_requires='>=3.6, <4',
-    version='0.0.12',
+    version='0.0.13',
     description='TEI Reader',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Sheean Spoel (Digital Humanities Lab, Utrecht University)',
     author_email='s.j.j.spoel@uu.nl',
     url='https://github.com/UUDigitalHumanitieslab/tei_reader',
